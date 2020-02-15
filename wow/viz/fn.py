685e02c90434c49cc3a05fef7d160d4f30ef83d0
gabrielarpino <arpino.gabriel@gmail.com>
2020-02-15 22:18:28 +0100

    Began random polynomials, part of visual functional analysis

---
+++ wow/viz/fn.py
@@ -0,0 +1,14 @@
+from multipledispatch import dispatch
+import stheno
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from .colour import PAL
+a = (list, np.ndarray)
+
+@dispatch(np.ndarray, function)
+def viz(x, f):
+    # Now condition on the observations to make predictions.
+    plt.plot(x, f(x), c=PAL[0])
+    plt.show()
+    return
